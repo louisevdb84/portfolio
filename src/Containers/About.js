43ce0cf429c396a76a157b0badb56bd92b8ed054
Louise van den Berg <louisevdb84@gmail.com>
2018-09-21 16:35:04 +0200

    Update online resume

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -12,9 +12,8 @@ const About = () => {
                 </div>  
                 <div className = "aboutText">
                     <p>I am a hardworking, passionate Web Developer whom graduated June/July 2017. I obtained a BSc Degree in Computing with Cum Laude.</p>
-                    <p>Since January 2018 I started web developer courses through UDEMY where I’ve learned React.js, Node.js, PostgreSQL etc. I also have about 2 years self-taught experience in C#, ASP.Net MVC and MSSQL and 1 year and 5 months working experience in a developer environment as a document developer. I've worked for about 5 years in total for different companies as a bookkeeper, and about 3 years in admin positions.</p>
+                    <p>Since January 2018 I started web developer courses through UDEMY where I’ve learned React.js, Angular, Node.js, PostgreSQL etc. I also have about 2 years self-taught experience in C#, ASP.Net MVC and MSSQL and 1 year and 5 months working experience in a developer environment as a document developer. I've worked for about 5 years in total for different companies as a bookkeeper, and about 3 years in admin positions.</p>
                     <p>I am analytical, hardworking and enjoy problem solving. I am flexible, quick to pick up new skills and eager to learn from others. </p>
-
                 </div>            
             </div>        
         
