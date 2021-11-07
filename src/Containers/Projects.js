f7b3b10d25c4ab70cae1c28da524bc08f6c9183f
Louise van den Berg <louisevdb84@gmail.com>
2021-11-07 16:08:11 +0100

    text updates

--- src/Containers/Projects.js
+++ src/Containers/Projects.js
@@ -50,23 +50,8 @@ const featuredDb = [
 const db = [
 
 
-    {
-        name: "Speech to Text Journal",
-        description: "This application uses speech recognition to convert your words into a journal entry. This app is also a coding challenge winner (Udemy Zero to Mastery)",
-        date: "March 2018",
-        tools: "React.js, UI-Router/React, CSS, Node.js, Express Framework, Bootstrap, Creative Tim",
-        image: journal,
-        link: "https://louisevdb84.github.io/journal-application/#/"
-    },
 
-    {
-        name: "NASA API App",
-        description: "Very basic app using NASA API to explore React Redux",
-        date: "August 2019",
-        tools: "React.js, Redux, Redux-Thunk, Redux-logger, Tachyons",
-        image: nasa,
-        link: "https://louisevdb84.github.io/nasa/"
-    },
+
     {
         name: "Online Portfolio",
         description: "The portfolio you are currently looking at",
@@ -76,12 +61,28 @@ const db = [
         link: "https://louisevdb84.github.io/portfolio"
     },
     {
+        name: "Speech to Text Journal",
+        description: "This application uses speech recognition to convert your words into a journal entry. This app is also a coding challenge winner (Udemy Zero to Mastery)",
+        date: "March 2018",
+        tools: "React.js, UI-Router/React, CSS, Node.js, Express Framework, Bootstrap, Creative Tim",
+        image: journal,
+        link: "https://louisevdb84.github.io/journal-application/#/"
+    },
+    {
         name: "Shopping List",
         description: "This app keeps track of your shopping list. ",
         date: "October 2018",
         tools: "Angular, JWT, Node.js, Express Framework, MongoDB",
         image: shoppinglist,
         link: "https://louisevdb84.github.io/shopping-list/"
+    },
+    {
+        name: "NASA API App",
+        description: "Very basic app using NASA API to explore React Redux",
+        date: "August 2019",
+        tools: "React.js, Redux, Redux-Thunk, Redux-logger, Tachyons",
+        image: nasa,
+        link: "https://louisevdb84.github.io/nasa/"
     },
 
 
@@ -92,11 +93,8 @@ const App = () => {
     return (
         <div className="animated zoomInRight cards proj" >
             <h1> My Projects </h1>
-            <hr></hr>
-            <h3> Featured Projects </h3>
-            <ProjectList projects={featuredDb} />
-            <hr></hr>
-            <h3>Other Projects </h3>
+            <hr></hr>            
+            <ProjectList projects={featuredDb} />                       
             <ProjectList projects={db} />
         </div>
     )
